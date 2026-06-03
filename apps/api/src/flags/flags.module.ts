ac9f747b5f7c2af59069da9e6005e2e6e3a73869
Daniel-VergaraM <d.vergaram2@uniandes.edu.co>
2026-06-03 18:10:36 -0500

    feat(api evaluator): forgot about this

--- apps/api/src/flags/flags.module.ts
+++ apps/api/src/flags/flags.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FlagsService } from './flags.service';
 import { FlagsController } from './flags.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { MessagingModule } from '../messaging/messaging.module';
 
 @Module({
+  imports: [PrismaModule, MessagingModule],
   controllers: [FlagsController],
   providers: [FlagsService],
+  exports: [FlagsService],
 })
 export class FlagsModule {}
