d9e38e0f04a988c344d3a06373a571128768660e
Daniel-VergaraM <d.vergaram2@uniandes.edu.co>
2026-06-10 18:36:27 -0500

    fix(unused-var): body from evaluate controller

--- apps/api/src/evaluate/evaluate.controller.ts
+++ apps/api/src/evaluate/evaluate.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class EvaluateController {
   @Post('evaluate')
   @Throttle({ default: { limit: 1000, ttl: 60000 } })
-  evaluate(@Body() body: any) {
+  evaluate() {
     return { enabled: true };
   }
 }
